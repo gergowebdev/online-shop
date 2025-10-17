c2e5cb531336551f4664292bf745003014e60aa1
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-10-17 17:47:05 +0200

    Creating the shop structure & registering the routes

---
+++ controllers/admin.js
@@ -0,0 +1,28 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
+    });
+  });
+};
