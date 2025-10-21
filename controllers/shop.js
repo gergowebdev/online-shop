a84c093e42676d9fba8bd41259a7abe4d43a569d
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-10-21 12:44:43 +0200

    Adding the product delete functionality, Displaying cart items on the Cart page

--- controllers/shop.js
+++ controllers/shop.js
@@ -34,9 +34,25 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart',
+  Cart.getCart((cart) => {
+    // need more information about the products too
+    Product.fetchAll((products) => {
+      // filter out the products, which are actually in the cart
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts,
+      });
+    });
   });
 };
 exports.postCart = (req, res, next) => {
