5658566ec1868cdbac6b70c783470643fc180830
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-10-21 15:20:42 +0200

    Deleting Cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -63,6 +63,15 @@ exports.postCart = (req, res, next) => {
   res.redirect('/cart');
 };
 
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  // we need the price, so we need the product information first
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
+};
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
--- models/cart.js
+++ models/cart.js
@@ -44,12 +44,16 @@ module.exports = class Cart {
       const updatedCart = { ...JSON.parse(fileContent) };
       // how often we do have the product
       const product = updatedCart.products.find((prod) => prod.id === id);
+      if (!product) {
+        return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
         (prod) => prod.id !== id
       );
       updatedCart.totalPrice =
         updatedCart.totalPrice - productPrice * productQty;
+
       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
         console.log(err);
       });
